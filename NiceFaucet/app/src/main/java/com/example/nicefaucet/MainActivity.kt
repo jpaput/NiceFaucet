56733706fca239b8092b0f3fff28be39f4133191
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-02 12:07:44 +0100

    firebase project

--- NiceFaucet/app/src/main/java/com/example/nicefaucet/MainActivity.kt
+++ NiceFaucet/app/src/main/java/com/example/nicefaucet/MainActivity.kt
@@ -1,32 +1,48 @@
 package com.example.nicefaucet
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nicefaucet.R.id.viewVideo
+import com.google.firebase.auth.FirebaseAuth
+import java.lang.Thread.sleep
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var mAuth: FirebaseAuth? = null
+
+    //animation value
+    private val animDuration = 3000 //time in ms
 
     //declaring  a null variable for VideoView
     var videoView: VideoView? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setTheme(R.style.Theme_NiceFaucet)
         setContentView(R.layout.activity_main)
 
+        mAuth = FirebaseAuth.getInstance()
+
         // assigning id of VideoView from
         // activity_main.xml layout file
-        videoView = findViewById<View>(R.id.viewVideo) as VideoView
+        videoView = findViewById<View>(viewVideo) as VideoView
 
-         }
+        // val splashFragment = SplashFragment()
+        //  val signInFragment = SignInFragment()
 
+        //  supportFragmentManager.beginTransaction().apply {
+        //        replace(nav_host_fragment_container, splashFragment)
+        //         commit()
+    }
     override fun onResume() {
         super.onResume()
 
+        animDuration
         // set the absolute path of the video file which is going to be played
         videoView!!.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.animation1))
 
@@ -37,12 +53,14 @@ class MainActivity : AppCompatActivity() {
 
         videoView!!.setOnCompletionListener {
             //TODO go to next fragment (login)
+            // After 5 seconds redirect to another intent
 
+            sleep(5 * 1000)
+            val i = Intent(baseContext, SignInFragment::class.java)
+            startActivity(i)
         }
 
     }
 
 }
-
-
 
--- NiceFaucet/app/src/main/java/com/example/nicefaucet/SplashFragment.kt
+++ NiceFaucet/app/src/main/java/com/example/nicefaucet/SplashFragment.kt
@@ -1,46 +1,53 @@
 package com.example.nicefaucet
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MainFragment : Fragment() {
+class SplashFragment : Fragment() {
+    /*  override fun onAttach(context: Context) {
+          super.onAttach(context)
+      }
+
+      //declaring  a null variable for VideoView
+      var videoView: VideoView? = null
+
+      //animation value
+      private val animDuration = 3000 //time in ms
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+      }
+
+      override fun onCreateView(
+          inflater: LayoutInflater, container: ViewGroup?,
+          savedInstanceState: Bundle?
+      ): View? {
+
+          // Inflate the layout for this fragment
+          return inflater.inflate(R.layout.fragment_splash, container, false)
+
+      }
 
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
-    }
+      override fun onResume() {
+          super.onResume()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance() =
-            MainFragment().apply {
+          animDuration
 
-            }
-                }
-    }
+
+          // set the absolute path of the video file which is going to be played
+          videoView!!.setVideoURI(Uri.parse("android.resource://" + (context?.packageName ) + "/" + R.raw.animation1))
+
+          videoView!!.requestFocus()
+
+          // starting the video
+          videoView!!.start()
+
+          videoView!!.setOnCompletionListener {
+              //TODO go to next fragment (login)
+          }
+
+      }*/
+
+}
+
