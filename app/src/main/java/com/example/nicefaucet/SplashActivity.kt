e42c0e0f40fcb14a9ba79b624bc791199835e199
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-08 09:52:17 +0100

    LoginScreen in progress

--- app/src/main/java/com/example/nicefaucet/SplashActivity.kt
+++ app/src/main/java/com/example/nicefaucet/SplashActivity.kt
@@ -6,13 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
 import android.widget.VideoView
 
 class SplashActivity : AppCompatActivity() {
 
     private var userIsSignedIn = false
     //animation value
-    private val animDuration = 3000 //time in ms
+    private val animDuration = 1000 //time in ms
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +30,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         val videoView = findViewById<VideoView>(R.id.videoView)
-
+        val textView: TextView = findViewById(R.id.logo)
         animDuration
         // set the absolute path of the video file which is going to be played
 
