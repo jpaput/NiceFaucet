7ff120c46513d002ef17a4b948bf67c951691f75
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-09 12:36:38 +0100

    fadeIn Logo done

--- app/src/main/java/com/example/nicefaucet/SplashActivity.kt
+++ app/src/main/java/com/example/nicefaucet/SplashActivity.kt
@@ -6,34 +6,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.VideoView
+import androidx.core.graphics.rotationMatrix
 
 class SplashActivity : AppCompatActivity() {
 
     private var userIsSignedIn = false
-    //animation value
-    private val animDuration = 1000 //time in ms
 
 
+    lateinit var fadeIn:Animation
+  //  lateinit var fadeOut:Animation
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setTheme(R.style.Theme_NiceFaucet)
         setContentView(R.layout.activity_splash)
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
 
+
+        val imageView = findViewById<ImageView>(R.id.logo)
+        //Animations
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in)
+    //    fadeOut = AnimationUtils.loadAnimation(this,R.anim.fade_out)
+
+
+        imageView.animation = fadeIn
+      //  imageView.animation = fadeOut
     }
+
 
     override fun onResume() {
         super.onResume()
         val videoView = findViewById<VideoView>(R.id.videoView)
 
-        animDuration
-        // set the absolute path of the video file which is going to be played
 
+
+        // set the absolute path of the video file which is going to be played
         videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.animation1))
 
         videoView.requestFocus()
