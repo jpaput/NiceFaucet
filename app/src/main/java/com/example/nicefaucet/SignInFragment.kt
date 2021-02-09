1a7cd3bdca6bb3426992d2f02bde78a92252b91e
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-09 15:40:23 +0100

    fadeIn Logo done

--- app/src/main/java/com/example/nicefaucet/SignInFragment.kt
+++ app/src/main/java/com/example/nicefaucet/SignInFragment.kt
@@ -14,6 +14,7 @@ class SignInFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
