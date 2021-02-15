155d7cae5147e5f1b3db95cd48871aacc634528f
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-15 18:08:31 +0100

    SignInActivity in progress

--- app/src/main/java/com/example/nicefaucet/MainActivity.kt
+++ app/src/main/java/com/example/nicefaucet/MainActivity.kt
@@ -1,29 +1,42 @@
 package com.example.nicefaucet
 
+
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import android.widget.VideoView
+import android.os.Handler
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat.postDelayed
 import com.google.firebase.auth.FirebaseAuth
-import java.lang.Thread.sleep
+import java.util.logging.Handler as LoggingHandler
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    private var mAuth: FirebaseAuth? = null
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setTheme(R.style.Theme_NiceFaucet)
         setContentView(R.layout.activity_main)
 
+
         mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
 
 
+        //if the user is not authenticated, send him to SignInActivity to authenticate first
+        //else send him to the dashboard
+        Handler().postDelayed( {
+            if (user !=null){
+                var dashboardIntent = Intent (this, DashBoardFragment ::class.java)
+                startActivity(dashboardIntent)
+            }else{
+                val signIntent = Intent(this, SignInActivity::class.java)
+                startActivity(signIntent)
+            }
+        }, 2000)
     }
-
 }
 
