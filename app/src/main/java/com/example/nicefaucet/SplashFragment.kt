355d03b250d281d1a54b09b1fee367966003b561
julien <j.paput@gmail.com>
2021-03-03 13:05:37 +0100

    Restructuration total du projet

---
+++ app/src/main/java/com/example/nicefaucet/SplashFragment.kt
@@ -0,0 +1,53 @@
+package com.example.nicefaucet
+
+import androidx.fragment.app.Fragment
+
+
+class SplashFragment : Fragment() {
+    /*  override fun onAttach(context: Context) {
+          super.onAttach(context)
+      }
+
+      //declaring  a null variable for VideoView
+      var videoView: VideoView? = null
+
+      //animation value
+      private val animDuration = 3000 //time in ms
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+      }
+
+      override fun onCreateView(
+          inflater: LayoutInflater, container: ViewGroup?,
+          savedInstanceState: Bundle?
+      ): View? {
+
+          // Inflate the layout for this fragment
+          return inflater.inflate(R.layout.fragment_splash, container, false)
+
+      }
+
+
+      override fun onResume() {
+          super.onResume()
+
+          animDuration
+
+
+          // set the absolute path of the video file which is going to be played
+          videoView!!.setVideoURI(Uri.parse("android.resource://" + (context?.packageName ) + "/" + R.raw.animation1))
+
+          videoView!!.requestFocus()
+
+          // starting the video
+          videoView!!.start()
+
+          videoView!!.setOnCompletionListener {
+              //TODO go to next fragment (login)
+          }
+
+      }*/
+
+}
+
